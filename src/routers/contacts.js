31eae3e85b8ff12af84a5d1a29fbea3133f62400
Maria Khaklenko <maria.khalenko@gmail.com>
2024-06-16 22:54:52 +0300

    hw4-validation2

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -10,19 +10,20 @@ import {
   validateCreateContact,
   validateUpdateContact,
 } from '../controllers/contacts.js';
-
+import { isValidId } from '../middleware/ValidId.js';
 import { ctrlWrapper } from '../middleware/Contacts.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsAll));
-router.get('/contacts/:contactId', ctrlWrapper(getByIdContact));
+router.get('/contacts/:contactId', isValidId, ctrlWrapper(getByIdContact));
 router.post('/contacts', validateCreateContact, ctrlWrapper(createNewContact));
 router.patch(
   '/contacts/:contactId',
+  isValidId,
   validateUpdateContact,
   ctrlWrapper(updateContact),
 );
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContact));
+router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContact));
 
 export default router;

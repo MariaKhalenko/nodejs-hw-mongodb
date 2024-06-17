33c1ab1d442581a9f43615454da745a670127c95
Maria Khaklenko <maria.khalenko@gmail.com>
2024-06-17 17:04:06 +0300

    hw4-validation 3

--- src/middleware/ValidId.js
+++ src/middleware/ValidId.js
@@ -9,3 +9,27 @@ export const isValidId = (req, res, next) => {
 
   next();
 };
+
+export const isValidName = (req, res, next) => {
+  const { name } = req.body;
+  console.log('Validating name:', name);
+  const nameRegex = /^[a-zA-Z\s]+$/;
+  if (!name || typeof name !== 'string' || !nameRegex.test(name)) {
+    console.log('Name entered incorrectly');
+    return next(createError(404, 'Name entered incorrectly'));
+  }
+  next();
+};
+
+export const isValidPhoneNumber = (req, res, next) => {
+  const { phoneNumber } = req.body;
+  const phoneRegex = /^[0-9-]+$/;
+  if (
+    !phoneNumber ||
+    typeof phoneNumber !== 'string' ||
+    !phoneRegex.test(phoneNumber)
+  ) {
+    return next(createError(404, 'PhoneNumber entered incorrectly'));
+  }
+  next();
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -10,17 +10,29 @@ import {
   validateCreateContact,
   validateUpdateContact,
 } from '../controllers/contacts.js';
-import { isValidId } from '../middleware/ValidId.js';
+import {
+  isValidId,
+  isValidName,
+  isValidPhoneNumber,
+} from '../middleware/ValidId.js';
 import { ctrlWrapper } from '../middleware/Contacts.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsAll));
 router.get('/contacts/:contactId', isValidId, ctrlWrapper(getByIdContact));
-router.post('/contacts', validateCreateContact, ctrlWrapper(createNewContact));
+router.post(
+  '/contacts',
+  isValidName,
+  isValidPhoneNumber,
+  validateCreateContact,
+  ctrlWrapper(createNewContact),
+);
 router.patch(
   '/contacts/:contactId',
   isValidId,
+  isValidName,
+  isValidPhoneNumber,
   validateUpdateContact,
   ctrlWrapper(updateContact),
 );
